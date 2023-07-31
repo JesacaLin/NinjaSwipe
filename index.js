90a544f0942b43fd40df5957f0a77cf0badcfea9
JesacaLin <JesacaLin@gmail.com>
2023-07-31 16:26:34 -0400

    set up LocalStorage

--- index.js
+++ index.js
@@ -4,10 +4,24 @@ const inputBTN = document.querySelector("#input-btn");
 const inputEl = document.querySelector("#input-el");
 const ulEl = document.querySelector("#ul-el");
 
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
+
+//console.log(leadsFromLocalStorage);
+
 inputBTN.addEventListener("click", () => {
   myLeads.push(inputEl.value);
   inputEl.value = "";
+
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+
   renderLeads();
+
+  //console.log(localStorage.getItem("myLeads"));
 });
 
 function renderLeads() {
@@ -22,6 +36,9 @@ function renderLeads() {
     listItem.appendChild(anchorTag);
     ulEl.appendChild(listItem);
 
-    console.log(listItem);
+    //set localstorage here
+    // localStorage.setItem("MyLeads", JSON.stringify(myLeads[i]));
+
+    console.log(myLeads[i]);
   }
 }
